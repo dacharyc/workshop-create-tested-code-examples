42f78436cc5e3782e33981617dc555a320b92762
dacharyc <dachary.carey@mongodb.com>
2025-08-05 17:07:31 -0400

    Update output comparison func, unrelated tests

--- code-example-tests/javascript/driver/tests/aggregation/pipelines/tutorials.test.js
+++ code-example-tests/javascript/driver/tests/aggregation/pipelines/tutorials.test.js
@@ -3,14 +3,14 @@ import { loadGroupSampleData } from '../../../examples/aggregation/pipelines/gro
 import { loadUnwindSampleData } from '../../../examples/aggregation/pipelines/unwind/tutorial-setup.js';
 import { loadJoinOneToOneSampleData } from '../../../examples/aggregation/pipelines/join-one-to-one/tutorial-setup.js';
 import { loadJoinMultiFieldSampleData } from '../../../examples/aggregation/pipelines/join-multi-field/tutorial-setup.js';
-import { runTemplateApp } from '../../../examples/aggregation/pipelines/template-app.js';
+import { run } from '../../../examples/aggregation/pipelines/template-app.js';
 import { runFilterTutorial } from '../../../examples/aggregation/pipelines/filter/tutorial.js';
 import { runGroupTutorial } from '../../../examples/aggregation/pipelines/group/tutorial.js';
 import { runUnwindTutorial } from '../../../examples/aggregation/pipelines/unwind/tutorial.js';
 import { runJoinOneToOneTutorial } from '../../../examples/aggregation/pipelines/join-one-to-one/tutorial.js';
 import { runJoinMultiFieldTutorial } from '../../../examples/aggregation/pipelines/join-multi-field/tutorial.js';
 import { MongoClient } from 'mongodb';
-import unorderedArrayOutputMatches from '../../../utils/outputMatchesExampleOutput.js';
+import outputMatchesExampleOutput from '../../../utils/outputMatchesExampleOutput.js';
 
 describe('Aggregation pipeline filter tutorial tests', () => {
   afterEach(async () => {
@@ -23,7 +23,7 @@ describe('Aggregation pipeline filter tutorial tests', () => {
   });
 
   it('Should return a basic document when executing the template app', async () => {
-    const result = await runTemplateApp();
+    const result = await run();
     expect(result[0]['name']).toStrictEqual('sample2');
   });
 
@@ -31,7 +31,7 @@ describe('Aggregation pipeline filter tutorial tests', () => {
     await loadFilterSampleData();
     const result = await runFilterTutorial();
     const outputFilepath = 'aggregation/pipelines/filter/tutorial-output.sh';
-    const arraysMatch = unorderedArrayOutputMatches(outputFilepath, result);
+    const arraysMatch = outputMatchesExampleOutput(outputFilepath, result, 'unordered');
     expect(arraysMatch).toBe(true);
   });
 
@@ -39,7 +39,7 @@ describe('Aggregation pipeline filter tutorial tests', () => {
     await loadGroupSampleData();
     const result = await runGroupTutorial();
     const outputFilepath = 'aggregation/pipelines/group/tutorial-output.sh';
-    const arraysMatch = unorderedArrayOutputMatches(outputFilepath, result);
+    const arraysMatch = outputMatchesExampleOutput(outputFilepath, result, 'unordered');
     expect(arraysMatch).toBe(true);
   });
 
@@ -47,7 +47,7 @@ describe('Aggregation pipeline filter tutorial tests', () => {
     await loadUnwindSampleData();
     const result = await runUnwindTutorial();
     const outputFilepath = 'aggregation/pipelines/unwind/tutorial-output.sh';
-    const arraysMatch = unorderedArrayOutputMatches(outputFilepath, result);
+    const arraysMatch = outputMatchesExampleOutput(outputFilepath, result, 'unordered');
     expect(arraysMatch).toBe(true);
   });
 
@@ -56,7 +56,7 @@ describe('Aggregation pipeline filter tutorial tests', () => {
     const result = await runJoinOneToOneTutorial();
     const outputFilepath =
       'aggregation/pipelines/join-one-to-one/tutorial-output.sh';
-    const arraysMatch = unorderedArrayOutputMatches(outputFilepath, result);
+    const arraysMatch = outputMatchesExampleOutput(outputFilepath, result, 'unordered');
     expect(arraysMatch).toBe(true);
   });
 
@@ -65,7 +65,7 @@ describe('Aggregation pipeline filter tutorial tests', () => {
     const result = await runJoinMultiFieldTutorial();
     const outputFilepath =
       'aggregation/pipelines/join-multi-field/tutorial-output.sh';
-    const arraysMatch = unorderedArrayOutputMatches(outputFilepath, result);
+    const arraysMatch = outputMatchesExampleOutput(outputFilepath, result, 'unordered');
     expect(arraysMatch).toBe(true);
   });
 });
